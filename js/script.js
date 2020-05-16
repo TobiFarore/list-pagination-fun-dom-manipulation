c5192aa73408248a8dca55ba81d028f1c93c4043
TobiFarore <tobifarore@icloud.com>
2020-05-16 19:21:39 +0100

    Completed Challenge

--- js/script.js
+++ js/script.js
@@ -12,48 +12,53 @@
 const body = document.querySelector('body');
 
 
-// YOUR CODE GOES HERE!!! Do the steps below to complete this challenge
-
-// 1. Create a `H1` element and store it in a variable
-
-  // 1a. Set the `H1` element's `innerHTML` equal to "Fun DOM Manipulation"
-
-  // 1a. Set the `H1` element's `id` equal to "main-heading"
-
-  // 1b. Append the `H1` element to the `body` element - body variable created for you above
-
-
-
-// 2. Create a `DIV` element and store it in a variable
-
-  // 2a. Set the `DIV` element's `id` attribute equal to "boat-container"
-
-  // 2b. Set the `DIV` element's `class` attribute equal to "boat-container"
-
-  // 2c. Append the `DIV` element to the `body` element - body variable created for you above
+/** YOUR CODE GOES HERE!!! Do the steps below to complete this challenge
+ * * 1. Create a `H1` element and store it in a variable
+ * * * 1a. Set the `H1` element's `innerHTML` equal to "Fun DOM Manipulation" 
+ * * * 1a. Set the `H1` element's `id` equal to "main-heading"
+ * * * 1b. Append the `H1` element to the `body` element - body variable created for you above
+*/
+const h1 = document.createElement('h1');
+h1.innerHTML = 'Fun DOM Manipulation';
+h1.id = 'main-heading';
+body.appendChild(h1);
 
 
+/**
+ * * 2. Create a `DIV` element and store it in a variable
+ * * * 2a. Set the `DIV` element's `id` attribute equal to "boat-container"
+ * * * 2b. Set the `DIV` element's `class` attribute equal to "boat-container"
+ * * * 2c. Append the `DIV` element to the `body` element - body variable created for you above
+*/
+const div = document.createElement('div');
+div.id = 'boat-container';
+div.className = 'boat-container';
+body.appendChild(div);
 
-// 3. Create an `IMG` element and store it in a variable
+/**
+ * * 3. Create an `IMG` element and store it in a variable
+ * * * 3a. Set the `IMG` element's `src` attribute equal to "img/boat.png"
+ * * * 3b. Set the `IMG` element's `alt` attribute equal to "Tugboat"
+ * * * 3c. Set the `IMG` element's `id` attribute equal to "boat-img"
+ * * * 3d. Append the `IMG` element to the `DIV` element you created above
+*/
+const img1 = document.createElement('img');
+img1.src = 'img/boat.png';
+img1.alt = 'Tugboat';
+img1.id = 'boat-img';
+body.appendChild(img1);
 
-  // 3a. Set the `IMG` element's `src` attribute equal to "img/boat.png"
-
-  // 3b. Set the `IMG` element's `alt` attribute equal to "Tugboat"
-
-  // 3c. Set the `IMG` element's `id` attribute equal to "boat-img"
-
-  // 3d. Append the `IMG` element to the `DIV` element you created above
-
-
-
-// 4. Create another `IMG` element and store it in a variable
-
-  // 4a. Set the `IMG` element's `src` attribute equal to "img/mike.png"
-
-  // 4b. Set the `IMG` element's `alt` attribute equal to "Mike The Frog"
-
-  // 4c. Set the `IMG` element's `id` attribute equal to "mike"
-
-  // 4d. Set the `IMG` element's `class` attribute equal to "mike"
-
-  // 4e. Append the `IMG` element to the `body` element
+/**
+ * * 4. Create another `IMG` element and store it in a variable
+ * * * 4a. Set the `IMG` element's `src` attribute equal to "img/mike.png"
+ * * * 4b. Set the `IMG` element's `alt` attribute equal to "Mike The Frog"
+ * * * 4c. Set the `IMG` element's `id` attribute equal to "mike"
+ * * * 4d. Set the `IMG` element's `class` attribute equal to "mike"
+ * * * 4e. Append the `IMG` element to the `body` element
+*/
+const img2 = document.createElement('img');
+img2.src = 'img/mike.png';
+img2.alt = 'Mike The Frog';
+img2.id = 'mike';
+img2.className = 'mike';
+body.appendChild(img2);
